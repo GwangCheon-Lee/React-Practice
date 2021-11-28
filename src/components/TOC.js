553650ab74a801f93650d32138fbdce3721d2f58
GwangCheon-Lee <raitmid56@naver.com>
2021-11-28 22:49:04 +0900

    chapter 17.2

---
+++ src/components/TOC.js
@@ -0,0 +1,23 @@
+import { Component } from "react";
+
+class TOC extends Component {
+    render() {
+        console.log("TOC render");
+        let lists = [];
+        let data = this.props.data;
+        for (let i = 0; i < data.length; i++) {
+            lists.push(
+                <li key={data[i].id}>
+                    <a href={"/content/" + data[i].id}>{data[i].title}</a>
+                </li>
+            );
+        }
+        return (
+            <nav>
+                <ul>{lists}</ul>
+            </nav>
+        );
+    }
+}
+
+export default TOC;
