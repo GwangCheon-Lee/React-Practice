774f1ca09dd94830d8f6f414428670c6fe500c66
GwangCheon-Lee <raitmid56@naver.com>
2021-12-05 11:12:07 +0900

    Ajax 기능

--- src/App.js
+++ src/App.js
@@ -1,148 +1,131 @@
-import { Component } from "react";
-import Subject from "./components/Subject";
-import TOC from "./components/TOC";
-import ReadContent from "./components/ReadContent";
-import CreateContent from "./components/CreateContent";
-import UpdateContent from "./components/UpdateContent";
-import Control from "./components/Control";
-import "./App.css";
+import { render } from "@testing-library/react";
+import React, { Component } from "react";
+
+class Nav extends Component {
+    render() {
+        let listTag = [];
+        for (let i = 0; i < this.props.list.length; i++) {
+            let li = this.props.list[i];
+            listTag.push(
+                <li key={li.id}>
+                    <a
+                        href={li.id}
+                        data-id={li.id}
+                        onClick={(e) => {
+                            e.preventDefault();
+                            console.log("trigger");
+                            this.props.onClick(e.target.dataset.id);
+                        }}
+                    >
+                        {li.title}
+                    </a>
+                </li>
+            );
+        }
+        return (
+            <nav>
+                <ul>{listTag}</ul>
+            </nav>
+        );
+    }
+}
+
+class Article extends Component {
+    render() {
+        return (
+            <article>
+                <h2>{this.props.title}</h2>
+                {this.props.desc}
+            </article>
+        );
+    }
+}
+
+class NowLoading extends Component {
+    render() {
+        return <div>Now Loading ...</div>;
+    }
+}
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.max_content_id = 3;
-        this.state = {
-            mode: "welcome",
-            selected_content_id: 1,
-            subject: { title: "WEB", sub: "World Wide Web!" },
-            welcome: { title: "Welcome", desc: "Hello, React!!" },
-            contents: [
-                { id: 1, title: "HTML", desc: "HTML is for information" },
-                { id: 2, title: "CSS", desc: "CSS is for design" },
-                {
-                    id: 3,
-                    title: "JavaScript",
-                    desc: "JavaScript is for interactive",
-                },
-            ],
-        };
-    }
-    getReadContent() {
-        for (let i = 0; i < this.state.contents.length; i++) {
-            let data = this.state.contents[i];
-            if (data.id === this.state.selected_content_id) {
-                return data;
-            }
-        }
-    }
-    getContent() {
-        let _title,
-            _article,
-            _desc = null;
-        if (this.state.mode === "welcome") {
-            _title = this.state.welcome.title;
-            _desc = this.state.welcome.desc;
-            _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
-        } else if (this.state.mode === "read") {
-            let _content = this.getReadContent();
-            _article = (
-                <ReadContent
-                    title={_content.title}
-                    desc={_content.desc}
-                ></ReadContent>
+    state = {
+        article: {
+            item: { title: "Welcome", desc: "Hello, React & Ajax" },
+            isLoading: false,
+        },
+        list: { items: [], isLoading: false },
+    };
+    componentDidMount() {
+        let newList = Object.assign({}, this.state.list, { isLoading: true });
+        this.setState({ list: newList });
+        fetch("list.json")
+            .then(function (result) {
+                return result.json();
+            })
+            .then(
+                function (json) {
+                    console.log(json);
+                    this.setState({
+                        list: {
+                            items: json,
+                            isLoading: false,
+                        },
+                    });
+                }.bind(this)
             );
-        } else if (this.state.mode === "create") {
-            _article = (
-                <CreateContent
-                    onSubmit={function (_title, _desc) {
-                        this.max_content_id = this.max_content_id + 1;
-                        let _contents = Array.from(this.state.contents);
-                        _contents.push({
-                            id: this.max_content_id,
-                            title: _title,
-                            desc: _desc,
-                        });
-                        this.setState({
-                            contents: _contents,
-                            mode: "read",
-                            selected_content_id: this.max_content_id,
-                        });
-                    }.bind(this)}
-                ></CreateContent>
-            );
-        } else if (this.state.mode === "update") {
-            let _content = this.getReadContent();
-            _article = (
-                <UpdateContent
-                    data={_content}
-                    onSubmit={function (_id, _title, _desc) {
-                        let _contents = Array.from(this.state.contents);
-                        for (let i = 0; i < _contents.length; i++) {
-                            if (_contents[i].id === _id) {
-                                _contents[i] = {
-                                    id: _id,
-                                    title: _title,
-                                    desc: _desc,
-                                };
-                                break;
-                            }
-                        }
-                        this.setState({ contents: _contents, mode: "read" });
-                    }.bind(this)}
-                ></UpdateContent>
-            );
-        }
-        return _article;
     }
     render() {
-        console.log("App render");
+        let NavTag = null;
+        if (this.state.list.isLoading) {
+            NavTag = <NowLoading></NowLoading>;
+        } else {
+            NavTag = (
+                <Nav
+                    list={this.state.list.items}
+                    onClick={function (id) {
+                        let newArticle = Object.assign({}, this.state.article, {
+                            isLoading: true,
+                        });
+                        this.setState({ article: newArticle });
+                        fetch(id + ".json")
+                            .then(function (result) {
+                                return result.json();
+                            })
+                            .then(
+                                function (json) {
+                                    this.setState({
+                                        article: {
+                                            item: {
+                                                title: json.title,
+                                                desc: json.desc,
+                                            },
+                                            isLoading: false,
+                                        },
+                                    });
+                                }.bind(this)
+                            );
+                    }.bind(this)}
+                ></Nav>
+            );
+        }
+
+        let ArticleTag = null;
+        if (this.state.article.isLoading) {
+            ArticleTag = <NowLoading></NowLoading>;
+        } else {
+            ArticleTag = (
+                <Article
+                    title={this.state.article.item.title}
+                    desc={this.state.article.item.desc}
+                ></Article>
+            );
+        }
 
         return (
             <div className="App">
-                <Subject
-                    title={this.state.subject.title}
-                    sub={this.state.subject.sub}
-                    onChangePage={function () {
-                        this.setState({ mode: "welcome" });
-                    }.bind(this)}
-                ></Subject>
-                <TOC
-                    onChangePage={function (id) {
-                        this.setState({
-                            mode: "read",
-                            selected_content_id: Number(id),
-                        });
-                    }.bind(this)}
-                    data={this.state.contents}
-                ></TOC>
-                <Control
-                    onChangeMode={function (_mode) {
-                        if (_mode === "delete") {
-                            if (window.confirm("정말로 삭제하시겠습니까?")) {
-                                let _contents = Array.from(this.state.contents);
-                                for (let i = 0; i < _contents.length; i++) {
-                                    if (
-                                        _contents[i].id ===
-                                        this.state.selected_content_id
-                                    ) {
-                                        console.log("test");
-                                        _contents.splice(i, 1);
-                                        break;
-                                    }
-                                }
-                                this.setState({
-                                    mode: "welcome",
-                                    contents: _contents,
-                                });
-                            }
-                        } else {
-                            this.setState({
-                                mode: _mode,
-                            });
-                        }
-                    }.bind(this)}
-                ></Control>
-                {this.getContent()}
+                <h1>Web</h1>
+                {NavTag}
+                {ArticleTag}
             </div>
         );
     }
